bc05d30609faaf5e1127f6cba4637e83067521d7
Feihua <qufeihua@gmail.com>
2018-12-28 00:13:52 +0200

    database user

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports={
-    database:'mongodb://mrr7rhq:Qq462683@ds145194.mlab.com:45194/meanauth',
+    database:'mongodb://john:j123456@ds145194.mlab.com:45194/meanauth',
     secret:'yoursecret'
 }
